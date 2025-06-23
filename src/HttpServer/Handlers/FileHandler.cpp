da01c19027cf08454b9aded6d04421bc4dec5474
tesla33io <artemstavropolov@gmail.com>
2025-06-23 13:37:42 +0200

    Feat: split file io related code

---
+++ src/HttpServer/Handlers/FileHandler.cpp
@@ -0,0 +1,20 @@
+#include "src/HttpServer/HttpServer.hpp"
+#include "src/Utils/StringUtils.hpp"
+#include <fstream>
+
+std::string WebServer::getFileContent(std::string path) {
+	std::string content;
+	std::ifstream file;
+
+	file.open(path.c_str(), std::ios::in | std::ios::binary);
+	if (!file.is_open()) {
+		_lggr.error("Coudln't open the file (" + path + ")");
+		return std::string();
+	} else {
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		content = buffer.str();
+	}
+	return content;
+}
