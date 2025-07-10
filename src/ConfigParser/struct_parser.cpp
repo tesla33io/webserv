99dcd58c40f4ca9ed9be704e62b977f38ecfc7bd
Helene Tharrault <htharrau@c4r1s4.clusters.forty2.fr>
2025-07-10 17:44:34 +0200

    feat[config file]: general directive are inherited in the location blocks, multiple error directives handled correctly

--- src/ConfigParser/struct_parser.cpp
+++ src/ConfigParser/struct_parser.cpp
@@ -24,7 +24,8 @@ namespace ConfigParsing {
 				struct_parser(*node, servers, logger);
 
 			else if (node->name == "server") {
-				ServerConfig server;
+				ServerConfig	server;
+				LocConfig 		general_dir;
 
 				for (std::vector<ConfigNode>::const_iterator child = node->children.begin(); child != node->children.end(); ++child) {
 					if (child->name == "host") 
@@ -38,10 +39,16 @@ namespace ConfigParsing {
 					else if (child->name == "location") {
 						LocConfig location;
 						location.path = child->args[0];
-						handle_location(location, child->children);
-						server.locations.push_back (location);
+						ConfigParsing::handle_location(location, child->children);
+						server.locations.push_back(location);
+					}
+					else {
+						ConfigParsing::handle_loc_details(general_dir, *child);
 					}
 				}
+
+				ConfigParsing::inherit_gen_dir(server, general_dir);
+
 				logger.logWithPrefix(Logger::INFO, "Config parsing", 
   				  "Parsed server block on " + server.host + ":" +  su::to_string(server.port) + 
    				 " with " + su::to_string(server.locations.size()) + " location(s).");
@@ -57,40 +64,74 @@ namespace ConfigParsing {
 	}
 
 	void handle_location(LocConfig &location, const std::vector<ConfigNode> &children) {
-
 		for (std::vector<ConfigNode>::const_iterator child = children.begin(); child != children.end(); ++child ) {
-
-				if (child->name == "error_page")
-					location.error_pages = handle_error_page(child->args); // map
-				else if (child->name == "client_max_body_size") {
-					std::stringstream ss(child->args[0]);
-					ss >> location.client_max_body_size;
-				}
-				else if (child->name == "allow_methods") 
-					location.allow_methods = child->args;
-				else if (child->name == "return") 
-					location.return_url = child->args[0];
-				else if (child->name == "root") 
-					location.root = child->args[0];
-				else if (child->name == "autoindex") 
-					location.autoindex = (child->args[0] == "on");
-				else if (child->name == "cgi_path") 
-					location.cgi_path = child->args[0];
-				else if (child->name == "cgi_ext") 
-					location.cgi_ext = child->args;
-				else if (child->name == "index") 
-					location.index = child->args;
+			ConfigParsing::handle_loc_details(location, *child);
 		}
 	}
-
-	std::map<int, std::string> handle_error_page(const std::vector<std::string> &args) {
-		std::map<int, std::string> map;
+	
+	void handle_loc_details(LocConfig &location, const ConfigNode &child) {
+		if (child.name == "error_page")
+			handle_error_page(location.error_pages, child.args);
+		else if (child.name == "client_max_body_size") {
+			std::stringstream ss(child.args[0]);
+			ss >> location.client_max_body_size;
+		}
+		else if (child.name == "allow_methods") 
+			location.allow_methods = child.args;
+		else if (child.name == "return") 
+			location.return_url = child.args[0];
+		else if (child.name == "root") 
+			location.root = child.args[0];
+		else if (child.name == "autoindex") 
+			location.autoindex = (child.args[0] == "on");
+		else if (child.name == "cgi_path") 
+			location.cgi_path = child.args[0];
+		else if (child.name == "cgi_ext") 
+			location.cgi_ext = child.args;
+		else if (child.name == "index") 
+			location.index = child.args;
+	}
+	
+	
+	void handle_error_page(std::map<int, std::string>& error_pages, const std::vector<std::string>& args) {
 		std::string uri = args.back();
 		for (size_t i = 0; i < args.size() - 1; ++i) {
 			int code = std::atoi(args[i].c_str());
-			map[code] = uri;
+			error_pages[code] = uri;
 		}
-		return map;
 	}
 
+		
+	void inherit_gen_dir(ServerConfig &server, const LocConfig &general_dir) {
+		for (size_t i = 0; i < server.locations.size(); ++i) {
+			LocConfig &loc = server.locations[i];
+			if (loc.allow_methods.empty())
+				loc.allow_methods = general_dir.allow_methods;
+			if (loc.cgi_ext.empty())
+				loc.cgi_ext = general_dir.cgi_ext;		
+			if (loc.cgi_path.empty())
+				loc.cgi_path = general_dir.cgi_path;
+			if (loc.client_max_body_size == 0)
+				loc.client_max_body_size = general_dir.client_max_body_size;				
+			if (loc.return_url.empty())
+				loc.return_url = general_dir.return_url;
+			if (loc.root.empty())
+				loc.root = general_dir.root;		
+			if (loc.autoindex == 0)
+				loc.autoindex = general_dir.autoindex;
+			if (loc.index.empty())
+				loc.index = general_dir.index;				
+			ConfigParsing::inherit_error_pages(loc.error_pages, general_dir.error_pages);
+		}
+	}
+
+	void inherit_error_pages(std::map<int, std::string>& loc_map, const std::map<int, std::string>& general_map) {
+		for (std::map<int, std::string>::const_iterator it = general_map.begin(); it != general_map.end(); ++it) {
+			if (loc_map.find(it->first) == loc_map.end()) {
+				loc_map[it->first] = it->second;
+			}
+		}
+	}
+
+	
 }
